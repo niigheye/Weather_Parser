794c36a62c61c6837c74842ccecfef63c9a30971
Robocotik <alexander.starkin@gmail.com>
2024-03-22 12:05:23 +0300

    [ADDED] curl lib and dependencies

--- main.cpp
+++ main.cpp
@@ -1,24 +1,65 @@
 #include "main.h"
+#include <stdio.h>
+
+#include "curl/curl.h"
+#pragma comment(lib, "curllib-bcb.lib") // Для C++Builder
+// #pragma comment(lib,"curllib.lib")    // для VC++
 int main(int argc, char **argv)
 {
-    ifstream pass("../token.txt");
 
+    system("chcp 1251>nul");
+    ifstream pass("../token.txt");
     string token;
     string id = "524901";
     pass >> token;
     string request = "http://api.openweathermap.org/data/2.5/forecast?id=" + id + "&appid=" + token;
+    cout << "My request is " << request << endl;
     cout << token;
+
     pass.close();
 
+    //---------------------------------------------------------------------------
     CURL *curl;
     CURLcode res;
+    std::string readBuffer;
 
-    GtkApplication *app;
-    int status;
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
 
-    return status;
+        std::cout << readBuffer << std::endl;
+    }
+    return 0;
+
+    // CURL *curl;
+    // CURLcode res;
+
+    // curl = curl_easy_init();
+    // if (curl)
+    // {
+    //     /* First set the URL that is about to receive our POST. This URL can
+    //        just as well be a https:// URL if that is what should receive the
+    //        data. */
+    //     curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+
+    //     /* Perform the request, res will get the return code */
+    //     res = curl_easy_perform(curl);
+
+    //     /* always cleanup */
+    //     curl_easy_cleanup(curl);
+    // }
+    // return 0;
+    // GtkApplication *app;
+    // int status;
+    // app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    // g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    // status = g_application_run(G_APPLICATION(app), argc, argv);
+    // g_object_unref(app);
+
+    // return status;
 }
--- main.h
+++ main.h
@@ -1,56 +1,63 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <gtk/gtk.h>
-#include "libs/curl-8.6.0/include/curl/curl.h"
-// #include <curl/curl.h>
+#include <curl/curl.h>
+// #include "D:\\study\\BMSTU\\practise\\PKSH_ALADIN_CUR1\\libs\\curl-8.6.0\\include\\curl\\curl.h"
 
 using namespace std;
 
-static void print_hello (GtkWidget *widget, gpointer   data)
+static void print_hello(GtkWidget *widget, gpointer data)
 {
-  g_print ("Hello World\n");
+  g_print("Hello World\n");
 }
 
-static void activate (GtkApplication *app, gpointer        user_data)
+static void activate(GtkApplication *app, gpointer user_data)
 {
   GtkWidget *window;
   GtkWidget *grid;
   GtkWidget *button;
 
   /* create a new window, and set its title */
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "Window");
 
   /* Here we construct the container that is going pack our buttons */
-  grid = gtk_grid_new ();
+  grid = gtk_grid_new();
 
   /* Pack the container in the window */
-  gtk_window_set_child (GTK_WINDOW (window), grid);
+  gtk_window_set_child(GTK_WINDOW(window), grid);
 
-  button = gtk_button_new_with_label ("Button 1");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  button = gtk_button_new_with_label("Button 1");
+  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
 
   /* Place the first button in the grid cell (0, 0), and make it fill
    * just 1 cell horizontally and vertically (ie no spanning)
    */
-  gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), button, 0, 0, 1, 1);
 
-  button = gtk_button_new_with_label ("Button 2");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  button = gtk_button_new_with_label("Button 2");
+  g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
 
   /* Place the second button in the grid cell (1, 0), and make it fill
    * just 1 cell horizontally and vertically (ie no spanning)
    */
-  gtk_grid_attach (GTK_GRID (grid), button, 1, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), button, 1, 0, 1, 1);
 
-  button = gtk_button_new_with_label ("Quit");
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+  button = gtk_button_new_with_label("Quit");
+  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_window_destroy), window);
 
   /* Place the Quit button in the grid cell (0, 1), and make it
    * span 2 columns.
    */
-  gtk_grid_attach (GTK_GRID (grid), button, 0, 1, 2, 1);
+  gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 1);
 
-  gtk_window_present (GTK_WINDOW (window));
+  gtk_window_present(GTK_WINDOW(window));
+}
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
 }
